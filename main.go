12401d536846f94b117ee4351c7503b1aaa89589
kiyutink <kirill.kiiutin@contentful.com>
2023-04-03 18:09:49 +0200

    Remove redundant log

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/kiyutink/tipam/cmd"
@@ -10,7 +9,6 @@ import (
 func main() {
 	rootCmd := cmd.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }

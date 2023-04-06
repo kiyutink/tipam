999e9cb2e7ccda46d535e519238e4ca3cd6162b9
kiyutink <kirill.kiiutin@contentful.com>
2023-04-06 15:13:58 +0200

    Show more ranges in home view

--- tipam/home_view.go
+++ tipam/home_view.go
@@ -2,7 +2,7 @@ package tipam
 
 import "github.com/rivo/tview"
 
-const defaultCIDR = "10.0.0.0/8"
+var defaultCIDRs = []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16 "}
 
 func NewHomeView(vc ViewContext) *HomeView {
 	return &HomeView{vc}
@@ -17,14 +17,19 @@ func (hv *HomeView) Name() string {
 }
 
 func (hv *HomeView) Primitive() tview.Primitive {
-	cell := tview.NewTableCell(defaultCIDR)
-	cell.SetExpansion(1)
-
 	table := tview.NewTable()
-	table.SetCell(0, 0, cell).Select(0, 0)
+	cells := make([]*tview.TableCell, 3)
+
+	for i, CIDR := range defaultCIDRs {
+		cells[i] = tview.NewTableCell(CIDR)
+		cells[i].Expansion = 1
+		table.SetCell(i, 0, cells[i])
+	}
+
+	table.Select(0, 0)
 	table.SetSelectable(true, true)
 	table.SetSelectedFunc(func(row, column int) {
-		networkView := NewNetworkView(hv.vc, defaultCIDR, 7)
+		networkView := NewNetworkView(hv.vc, defaultCIDRs[row], 7)
 		hv.vc.PushView(networkView)
 	})
 

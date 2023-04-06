8cbff95decd5d6be014160403ba11656bdd69102
kiyutink <kirill.kiiutin@contentful.com>
2023-04-06 14:48:22 +0200

    Rename vars

--- tipam/tipam.go
+++ tipam/tipam.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Tipam struct {
-	MainVC *ViewContext
-	App    *tview.Application
+	ViewContext *ViewContext
+	App         *tview.Application
 }
 
 func InitTipam() {
 	app := tview.NewApplication()
-	mainVCPages := tview.NewPages()
-	mainVCPages.SetBorder(true)
+	pages := tview.NewPages()
+	pages.SetBorder(true)
 
 	text := tview.NewTextView()
 	text.SetText("Id ex non minim laboris Lorem reprehenderit Lorem qui enim irure eu. Id cillum aliqua dolor ipsum enim esse adipisicing officia. Sint reprehenderit aute elit consectetur qui anim aute ullamco eu eiusmod aliqua. Proident duis cillum labore nisi qui commodo occaecat amet cillum laboris laborum sint laboris. Minim amet excepteur nisi eu velit exercitation veniam do pariatur pariatur nisi.")
@@ -22,24 +22,24 @@ func InitTipam() {
 	grid := tview.NewGrid()
 	grid.SetRows(5, 0)
 	grid.AddItem(text, 0, 0, 1, 1, 0, 0, false)
-	grid.AddItem(mainVCPages, 1, 0, 1, 1, 0, 0, true)
+	grid.AddItem(pages, 1, 0, 1, 1, 0, 0, true)
 
 	app.SetRoot(grid, true)
-	app.SetFocus(mainVCPages)
+	app.SetFocus(pages)
 
 	viewStack := helper.NewStack[View]()
 
-	mainVC := &ViewContext{
+	viewContext := &ViewContext{
 		ViewStack: viewStack,
-		Pages:     mainVCPages,
+		Pages:     pages,
 		Storage:   map[string]string{}, // TODO: temp
 	}
 
-	mainVC.PushView(NewHomeView(*mainVC))
+	viewContext.PushView(NewHomeView(*viewContext))
 
 	tipam := &Tipam{
-		MainVC: mainVC,
-		App:    app,
+		ViewContext: viewContext,
+		App:         app,
 	}
 
 	tipam.App.Run()

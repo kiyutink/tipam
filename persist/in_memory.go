26405e52cda6749293761ebc02ef8409bca1df37
kiyutink <kiyutin@gmail.com>
2023-04-19 23:31:37 +0200

    Add InMemory persistor

---
+++ persist/in_memory.go
@@ -0,0 +1,28 @@
+package persist
+
+import "github.com/kiyutink/tipam/tipam"
+
+type InMemory struct {
+	state *tipam.State
+}
+
+func (im *InMemory) Persist(s *tipam.State) error {
+	im.state = s
+	return nil
+}
+
+func (im *InMemory) Read() (*tipam.State, error) {
+	if im.state == nil {
+		return tipam.NewState(), nil
+	}
+
+	return im.state, nil
+}
+
+func (im *InMemory) Lock() error {
+	return nil
+}
+
+func (im *InMemory) Unlock() error {
+	return nil
+}

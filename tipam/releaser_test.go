581f5463a9a12f321105be95f6867103fdcb3d0d
kiyutink <kirill.kiiutin@contentful.com>
2023-04-20 17:31:35 +0200

    Add test for releaser

---
+++ tipam/releaser_test.go
@@ -0,0 +1,20 @@
+package tipam
+
+import "testing"
+
+func TestRelease(t *testing.T) {
+	cidr := "10.0.0.0/8"
+	c := MustParseClaimFromCIDR(cidr, []string{"test"}, false)
+	state := NewStateWithClaims([]*Claim{c})
+	p := newTestPersistor(state)
+
+	tr := NewRunner(p, RunnerOpts{})
+	err := tr.Release(cidr)
+	if err != nil {
+		t.Errorf("error running release on %v: %v", cidr, err)
+	}
+
+	if _, ok := state.Claims[cidr]; ok {
+		t.Errorf("expected release to remove claim from state, but didn't")
+	}
+}

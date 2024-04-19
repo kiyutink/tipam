8b081d98123bf5ced970d0270ac6d621d1710586
kiyutink <kirill.kiiutin@contentful.com>
2024-04-19 16:59:00 +0200

    Add better error message rendering in claim_view

--- visual/claim_view.go
+++ visual/claim_view.go
@@ -1,7 +1,6 @@
 package visual
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/gdamore/tcell/v2"
@@ -10,57 +9,63 @@ import (
 )
 
 type ClaimView struct {
-	viewContext *ViewContext
-	cidr        string
+	viewContext   *ViewContext
+	cidr          string
+	validationErr string
 }
 
 func NewClaimView(vc *ViewContext, cidr string) *ClaimView {
 	return &ClaimView{
-		viewContext: vc,
-		cidr:        cidr,
+		viewContext:   vc,
+		cidr:          cidr,
+		validationErr: "",
 	}
 }
 
-func (rv *ClaimView) Name() string {
+func (cv *ClaimView) Name() string {
 	return ""
 }
 
-func (rv *ClaimView) Meta() string {
+func (cv *ClaimView) Meta() string {
 	return ""
 }
 
-func (rv *ClaimView) Primitive() tview.Primitive {
+func (cv *ClaimView) Primitive() tview.Primitive {
 	tagsInputVal := ""
 
 	form := tview.NewForm()
 	form.SetBorder(true)
 
-	form.AddTextView("CIDR", rv.cidr, 40, 1, false, false)
+	form.AddTextView("CIDR", cv.cidr, 40, 1, false, false)
 	form.AddInputField("Tags (separated with /)", "", 0, nil, func(text string) {
 		tagsInputVal = text
 	})
+	if cv.validationErr != "" {
+		form.AddTextView("Error", cv.validationErr, 40, 3, false, false)
+	}
 	form.AddButton("Claim", func() {
 		tags := strings.Split(tagsInputVal, "/")
 		// TODO: 'final' param is hardcoded, should be passed in the form
-		err := rv.viewContext.Runner.Claim(tipam.MustParseClaimFromCIDR(rv.cidr, tags, false), tipam.ClaimOpts{})
+		err := cv.viewContext.Runner.Claim(tipam.MustParseClaimFromCIDR(cv.cidr, tags, false), tipam.ClaimOpts{})
 		// TODO: Is this the best way to do this (refresh local state)? Probably not?
-		rv.viewContext.State, _ = rv.viewContext.Runner.ReadState()
+		cv.viewContext.State, _ = cv.viewContext.Runner.ReadState()
 		if err != nil {
-			fmt.Println(err)
+			cv.validationErr = err.Error()
+			cv.viewContext.DrawModal()
 			return
 		}
-		cl := tipam.MustParseClaimFromCIDR(rv.cidr, tags, false)
-		rv.viewContext.State.Claims[rv.cidr] = cl
-		rv.viewContext.HideModal()
-		rv.viewContext.Draw()
+		cl := tipam.MustParseClaimFromCIDR(cv.cidr, tags, false)
+		cv.viewContext.State.Claims[cv.cidr] = cl
+		cv.viewContext.HideModal()
+		cv.viewContext.Draw()
 	})
 
 	form.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyESC {
-			rv.viewContext.HideModal()
+			cv.viewContext.HideModal()
 		}
 		return event
 	})
 
-	return newModal(form, 40, 10)
+	return newModal(form, 60, 13)
 }

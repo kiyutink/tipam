6557b59bc780001b90cd8616debe7ea62cbb320b
kiyutink <kiyutin@gmail.com>
2025-04-20 13:02:28 +0200

    Add missing log to releaser

--- tipam/releaser.go
+++ tipam/releaser.go
@@ -1,6 +1,11 @@
 package tipam
 
 func (r *Runner) Release(cidr string) error {
+	err := r.persistor.Lock()
+	if err != nil {
+		return err
+	}
+	defer r.persistor.Unlock()
 	state, err := r.persistor.Read()
 	if err != nil {
 		return err
